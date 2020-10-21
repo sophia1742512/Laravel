9f923b9920fc65906056940cd383aaff0bb8158f
”AYANO” <ec2-user@ip-172-31-23-129.us-east-2.compute.internal>
2020-10-21 23:42:13 +0000

    課題提出08

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    public function edit()
+    {
+        return view('admin.profle.edit');
+    }
+}
