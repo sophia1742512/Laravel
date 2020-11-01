98c6fe5e50aee782d5b36da145f2c0bcec1d2c3d
”AYANO” <ec2-user@ip-172-31-23-129.us-east-2.compute.internal>
2020-11-01 11:54:34 +0000

    課題提出10

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="vewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyProfile</title>
+        <body>
+            <h1>プロフィール編集画面</h1>
+        </body>
+    </head>
+</html>
